6fbe031eca2794cb063879fe2aa289fea3a2111f
Marcin Sulecki <marcin.sulecki@altkom.pl>
2019-03-27 10:11:10 +0100

    Day 3

--- Motorola.MotoTaxi.Orders.Api/Program.cs
+++ Motorola.MotoTaxi.Orders.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,8 +18,30 @@ namespace Motorola.MotoTaxi.Orders.Api
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        // https://codingblast.com/asp-net-core-configuration/
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigureConfiguration)
                 .UseStartup<Startup>();
+
+
+
+        private static void ConfigureConfiguration(
+            WebHostBuilderContext context, 
+            IConfigurationBuilder config)
+        {
+
+            var env = context.HostingEnvironment;
+
+            // Trace.WriteLine(context.HostingEnvironment.EnvironmentName);
+
+            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            config.AddXmlFile("appsettings.xml", optional: false, reloadOnChange: true);
+            config.AddXmlFile($"appsettings.{env.EnvironmentName}.xml", optional: true, reloadOnChange: true);
+
+
+        }
     }
 }
